d491b6890d603e58573f1c6b88b89043ddc34361
Rushhav <rushav182@gmail.com>
2026-01-12 10:58:47 +0530

    vpc-module-test

---
+++ provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.84.0"
+    }
+  }
+  backend "s3" {
+    bucket = "82s-tf-remote-state-dev-143"
+    key = "module-demo-vpc"
+    region = "us-east-1"
+    dynamodb_table = "82s-tf-remote-state-dev-143"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
